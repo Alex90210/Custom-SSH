4132955f0dfc76a19d71025d89cf81a24e0e0d65
Alex90210 <albertalexandru90210@gmail.com>
2024-01-08 13:42:58 +0200

    final (without user status)

--- SSHC/src/client.cpp
+++ SSHC/src/client.cpp
@@ -3,6 +3,14 @@
 
 void command_loop(const int& sd, std::string& username) {
 
+    // ANSI escape code for setting the color
+    const std::string RED = "\033[91m";
+    const std::string YELLOW = "\033[93m";
+    const std::string BLUE = "\033[94m";
+    const std::string GREEN = "\033[92m";
+    const std::string BLACK = "\033[00m";
+    const std::string CYAN = "\033[96m";
+    const std::string RESET = "\033[0m";
     std::string path {"/home/alex"};
     std::string server_output_delimiter {"@@@"};
     const char* user_n = username.c_str();
@@ -12,13 +20,15 @@ void command_loop(const int& sd, std::string& username) {
         char buf[1000];
         memset(buf, 0, sizeof(buf));
 
-        std::cout << user_n << " @ <" << path << "> :: ";
+        std::cout << YELLOW << user_n << GREEN << " @ " << GREEN << "<" << YELLOW << path
+        << GREEN << "> " << GREEN << ":: "  << RED;
         fflush(stdout);
 
         int bytes_num = read(0, buf, sizeof(buf));
         buf[bytes_num] = '\0';
         // printf("[Client]User input: %s", buf);
 
+        std::cout << RESET << std::flush;
         if (bytes_num > 0 && buf[bytes_num - 1] == '\n') {
             buf[bytes_num - 1] = '\0';
             bytes_num--;
